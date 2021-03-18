f5f05d09eb9429db77a33fbb769270e39a079fb9
Daniel Lee <47399650+iproudyou@users.noreply.github.com>
2021-03-17 20:50:58 -0400

    initialize postgres (#2)

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
-from flask import Flask
-app = Flask(__name__)
+import json
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+from flask import request
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+from . import create_app
+from .models import Cats, db
+
+app = create_app()
+
+@app.route('/', methods=['GET'])
+def fetch():
+    cats = db.get_all(Cats)
+    all_cats = []
+    for cat in cats:
+        new_cat = {
+            "id": cat.id,
+            "name": cat.name,
+            "price": cat.price,
+            "breed": cat.breed
+        }
+
+        all_cats.append(new_cat)
+
+    return json.dumps(all_cats), 200
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    name = data['name']
+    price = data['price']
+    breed = data['breed']
+
+    db.add_instance(Cats, name=name, price=price, breed=breed)
+    return json.dumps("Added"), 200
+
+@app.route('/remove/<cat_id>', methods=['DELETE'])
+def remove(cat_id):
+    db.delete_instance(Cats, id=cat_id)
+    return json.dumps("Deleted"), 200
+
+@app.route('/edit/<cat_id>', methods=['PATCH'])
+def edit(cat_id):
+    data = request.get_json()
+    new_price = data['price']
+    db.edit_instance(Cats, id=cat_id, price=new_price)
+    return json.dumps("Edited"), 200
