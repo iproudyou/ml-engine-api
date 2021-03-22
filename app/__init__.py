b2c1b662fe6ace3881e62047edf41e6c841f28c8
unknown <iproudyou@gmail.com>
2021-03-22 10:22:24 -0400

    add api-token

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 from app.models import db
-from app import config
+from app.config import BaseConfig
 
 def create_app():
     flask_app = Flask(__name__)
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] =config.DATABASE_CONNECTION_URI
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] =BaseConfig.DATABASE_CONNECTION_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
     db.init_app(flask_app)
