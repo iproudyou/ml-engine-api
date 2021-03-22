9869e27c6e23481c7d72bd685dbd7f899fd3ecc7
unknown <iproudyou@gmail.com>
2021-03-22 10:22:24 -0400

    add api-token

--- app/api.py
+++ app/api.py
@@ -1,14 +1,42 @@
 import json
-
-from flask import request, jsonify
+from flask import request, jsonify, make_response
+from flask_cors import CORS
 
 from app import create_app, database
 from app.models import Cats
+from app.helpers import encode_auth_token, decode_auth_token, is_unauthorized
+from app.helpers.logger import logger
 
 app = create_app()
 
+# auth api token
+# @app.before_request
+def server_api_auth():
+    try:
+        if 'x-api-token' in request.headers:
+            token = request.headers['x-api-token']
+            decode_auth_token(token)
+            logger.debug('ML-ENGINE-API SUCCESSFULLY ACCESSED')
+        else:
+            raise Exception('x-api-token not found')
+    except Exception as e:
+        logger.error(f'Error while authenticating API TOKEN: {e}')
+        return is_unauthorized()
+
+# override 404 error handler
+@app.errorhandler(404)
+def resource_not_found(error):
+    """This will be response returned if the user attempts to access
+    a non-existent respurce or url"""
+
+    response_payload = dict(
+        message = 'The requested URL was not found on the server.'
+    )
+    return make_response(jsonify(response_payload), 404)
+    
+    
 @app.route('/', methods=['GET'])
-def hello_world():
+def welcome_loco():
     return jsonify({'message': 'Welcome K-POP LOCO'})
 
 @app.route('/all', methods=['GET'])
@@ -48,6 +76,8 @@ def edit(cat_id):
     new_price = data['price']
     database.edit_instance(Cats, id=cat_id, price=new_price)
     return json.dumps("Edited"), 200
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
+"""App instance configs"""
+ 
 import os
 
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST']
-database = os.environ['POSTGRES_DB']
-port = os.environ['POSTGRES_PORT']
+class BaseConfig:
+    # Database config
+    user = os.environ['POSTGRES_USER']
+    password = os.environ['POSTGRES_PASSWORD']
+    host = os.environ['POSTGRES_HOST']
+    database = os.environ['POSTGRES_DB']
+    port = os.environ['POSTGRES_PORT']
+    DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+    # App config
+    API_SECRET_KEY = os.environ['API_SECRET_KEY']
